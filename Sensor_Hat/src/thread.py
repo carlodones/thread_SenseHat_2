542654dded5996f754aa8a565c3a292dc03b27fa
carlodones <36564232+carlodones@users.noreply.github.com>
2018-07-13 16:10:20 +0200

    Add files via upload

--- Sensor_Hat/src/thread.py
+++ Sensor_Hat/src/thread.py
@@ -11,9 +11,19 @@ class TestThread(threading.Thread):
         self.counter = counter
 
     def run(self):
+        #print ("threadID =" + str(self.threadID) ) 
+        #print ("name =" + str(self.name) )
+        #print ("counter =" +  str(self.counter) )
+
         print("Starting " + self.name)
-        print_time(self.name, self.counter, 5)
-        print("Exiting " + self.name)
+        if self.threadID == 1:
+            print_time(self.name, self.counter, 5)
+        
+        if self.threadID == 2:
+           print_counter(self.name, self.counter, 10)
+
+        if self.threadID == 3:
+            stampa_stupida(self.name, self.counter, 20)
 
 def print_time(threadName, delay, counter):
     while counter:
@@ -23,14 +33,29 @@ def print_time(threadName, delay, counter):
         print("%s: %s" % (threadName, time.ctime(time.time())))
         counter -= 1
 
+def print_counter(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        print(threadName, "ciclo", str(counter))
+        counter -= 1
 
+def stampa_stupida(threadName, delay, counter):
+    while counter:
+        print ("Contatore =" + str(counter) )
+        counter -= 1 
+        
 # Create new threads
 thread1 = TestThread(1, "Thread 1", 1)
 thread2 = TestThread(2, "Thread 2", 2)
+thread3 = TestThread(3, "Thread 3", 3)
 
 # Start new Threads
 thread1.start()
 thread2.start()
+thread3.start()
 thread1.join()
 thread2.join()
+thread3.join()
 print("Fine del main thread")
