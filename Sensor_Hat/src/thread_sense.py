1455d3252a5f0bf77ddf5d0951acd61692f47622
TIE <riccardoarona>
2018-07-19 18:28:11 +0200

    Scala cromatica

--- Sensor_Hat/src/thread_sense.py
+++ Sensor_Hat/src/thread_sense.py
@@ -1,7 +1,7 @@
 import threading
 import time
 from random import randint
-from sense_hat import SenseHat
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 sense = SenseHat()
 
 #Define the colours red and green
@@ -13,6 +13,17 @@ white = (255,255,255)
 blue = (0, 0, 255)
 
 exitFlag = 0
+
+VertPixels = [0, 1, 2, 3, 4, 5, 6, 7]
+HorzPixels = [0, 1, 2, 3, 4, 5, 6, 7]
+
+MaxTemp = 28
+MinTemp = 23
+
+def pushed_middle(event):
+    if event.action != ACTION_RELEASED:
+        exitFlag = 1
+        print("exit")
 
 class TestThread(threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -36,28 +47,30 @@ class TestThread(threading.Thread):
 #  DEFINIZIONE  THREAD  ID = 1
 #  Dichiarazione di tutte le azioni che devono essere svolte dal THREAD
 def acq_sensori(threadName, delay, counter):
-    
-    # background
-    bg = red
-      
-    # colore testo
-    tx = white
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        
+        # background
+        bg = red
+        
+        # colore testo
+        tx = white
 
-    # Lettura dai sensori del SenseHat acquisizione Temperatura, Pressione, Humidity
-    t = sense.get_temperature()
-    p = sense.get_pressure()
-    h = sense.get_humidity()
+        # Lettura dai sensori del SenseHat acquisizione Temperatura, Pressione, Humidity
+        t = sense.get_temperature()
 
-    # Arrotondamento ad una cifra decimale
-    t = round(t, 1)
-    p = round(p, 1)
-    h = round(h, 1)
+        # Arrotondamento ad una cifra decimale
+        t = round(t, 1)
 
-    # str() conversione valori int in string per poterli concatenare 
-    message = "Temperature: " + str(t) + "Pressure: " + str(p) + "Humidity: " + str(h)
+        # str() conversione valori int in string per poterli concatenare 
+        message = "Temperature: " + str(t)
 
-    # Visualizzazione messaggio scorrevole SenseHat
-    sense.show_message(message, text_colour=tx, scroll_speed=0.50, back_colour=bg)
+        show_temperature(t)
+
+        # Visualizzazione messaggio scorrevole SenseHat
+        # sense.show_message(message, text_colour=tx, scroll_speed=0.50, back_colour=bg)
 
 #  DEFINIZIONE  THREAD  ID = 2
 #  Dichiarazione di tutte le azioni che devono essere svolte dal THREAD
@@ -78,11 +91,20 @@ def print_counter(threadName, delay, counter):
         time.sleep(delay)
         print(threadName, "ciclo", str(counter))
         counter -= 1
-        
+
+def show_temperature(temp_value)
+    for vp in VertPixels
+        for hp in HorzPixels
+            dist_from_center = math.sqrt((vp - 3.5)*(vp - 3.5) + (hp - 3.5)*(hp - 3.5))
+            pixel_temp = temp_value * (3.5 - dist_from_center)
+            pixel_light = ((pixel_temp - MinTemp) / (MaxTemp - MinTemp)) * 255
+            sense.set_pixel(hp, vp, pixel_light, pixel_light, pixel_light)
+
+
 # Create new threads
-thread1 = TestThread(1, "Thread 1", 1)
-thread2 = TestThread(2, "Thread 2", 2)
-thread3 = TestThread(3, "Thread 3", 3)
+thread1 = TestThread(1, "Thread 1", 1000)
+thread2 = TestThread(2, "Thread 2", 1000)
+thread3 = TestThread(3, "Thread 3", 1000)
 
 # Start new Threads
 thread1.start()
