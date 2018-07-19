5a0430119a38756ce1ea85ca7d046b9e3ddc94da
TIE <riccardoarona>
2018-07-19 18:58:04 +0200

    Errore coordinate pixel

--- Sensor_Hat/src/thread_sense.py
+++ Sensor_Hat/src/thread_sense.py
@@ -38,12 +38,13 @@ class TestThread(threading.Thread):
         print("Starting " + self.name)
         if self.threadID == 1:
             acq_sensori(self.name, 1, self.counter)
-        
+"""        
         if self.threadID == 2:
             print_time(self.name, 1, self.counter)
 
         if self.threadID == 3:
             print_counter(self.name, 1, self.counter)
+"""
 
 #  DEFINIZIONE  THREAD  ID = 1
 #  Dichiarazione di tutte le azioni che devono essere svolte dal THREAD
@@ -59,11 +60,14 @@ def acq_sensori(threadName, delay, counter):
         # Arrotondamento ad una cifra decimale
         t = round(t, 1)
 
+        print("Temperatura: <" + t + ">")
+
         # Coloro il display in funzione della T rilevata
         show_temperature(t)
 
         counter -= 1
 
+""" 
 #  DEFINIZIONE  THREAD  ID = 2
 #  Dichiarazione di tutte le azioni che devono essere svolte dal THREAD
 def print_time(threadName, delay, counter):
@@ -82,27 +86,28 @@ def print_counter(threadName, delay, counter):
             threadName.exit()
         time.sleep(delay)
         print(threadName, "ciclo", str(counter))
-        counter -= 1
+        counter -= 1 
+"""
 
 def show_temperature(temp_value):
     for vp in VertPixels:
         for hp in HorzPixels:
             dist_from_center = math.sqrt((vp - 3.5)*(vp - 3.5) + (hp - 3.5)*(hp - 3.5))
             pixel_temp = temp_value * (3.5 - dist_from_center)
-            pixel_light = ((pixel_temp - MinTemp) / (MaxTemp - MinTemp)) * 255
+            pixel_light = int( (((pixel_temp - MinTemp) / (MaxTemp - MinTemp)) * 255) // 1)
             sense.set_pixel(hp, vp, pixel_light, pixel_light, pixel_light)
 
 
 # Create new threads
 thread1 = TestThread(1, "Thread 1", 50)
-thread2 = TestThread(2, "Thread 2", 50)
-thread3 = TestThread(3, "Thread 3", 50)
+# thread2 = TestThread(2, "Thread 2", 50)
+# thread3 = TestThread(3, "Thread 3", 50)
 
 # Start new Threads
 thread1.start()
-thread2.start()
-thread3.start()
+# thread2.start()
+# thread3.start()
 thread1.join()
-thread2.join()
-thread3.join()
+# thread2.join()
+# thread3.join()
 print("Fine del main thread")
