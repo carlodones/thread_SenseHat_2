f83f6a3fb0f722b73a17e9444908485aa39ca1e3
TIE <riccardoarona>
2018-08-02 10:45:57 +0200

    code split

---
+++ Sensor_Hat/src/mod_constants.py
@@ -0,0 +1,11 @@
+# Classe contenente le chiavi per la creazione del JSON
+class MQTTKeysConstant(object):
+    def __init__(self):
+        pass    
+
+    key_payload = "payload"
+    key_address = "address"
+    key_qos = "qos"
+    key_values = "values"
+    key_timestamp = "timestampDevice"
+    
--- Sensor_Hat/src/thread_sense.py
+++ Sensor_Hat/src/thread_sense.py
@@ -2,6 +2,8 @@ import threading
 import time
 import math
 import json
+import mod_measure_list
+import mod_constants
 from random import randint
 from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 sense = SenseHat()
@@ -33,107 +35,6 @@ def pushed_middle(event):
     if event.action == ACTION_PRESSED:
         print("Button pressed")
         exit_flag = 1
-
-# Classe contenente le chiavi per la creazione del JSON
-class KeysConstant(object):
-    def __init__(self):
-        pass    
-
-    key_payload = "payload"
-    key_address = "address"
-    key_qos = "qos"
-    key_values = "values"
-    key_timestamp = "timestampDevice"
-
-# Classe per la memorizzazione di una singola misura
-class Measure(object):
-    def __init__(self, channel, value, timestamp, count=1):
-        self.channel = channel
-        self.value = value
-        self.timestamp = timestamp
-        self.read = 0
-        self.average = 0
-        self.json = 0
-        self.count = count
-
-# Classe per la gestione delle liste misura
-class MeasureList(object):
-    def __init__(self):
-        pass
-
-    global kc
-    plist = []
-
-    # Aggiunge alla lista una misura dati i valori
-    def add_details(self, channel, value, timestamp):
-        meas = Measure(channel, value, timestamp)
-        self.plist.append(meas)
-
-    # Aggiunge alla lista una misura
-    def add_measure(self, meas):
-        self.plist.append(meas)
-
-    # Ritorna lista delle misure per singolo canale
-    def list_by_channel(self, channel):
-        part_list = []
-        for meas in self.plist:
-            if ((meas.channel == channel) & (meas.read == 0)):
-                part_list.append(meas)
-                meas.read = 1
-        return part_list
-
-    # Ritorna lista delle misure per singolo canale e stato
-    def json_dictionary(self):
-        dic_list = []
-        for meas in self.plist:
-            elem_dic = {}
-            if (meas.json == 0):
-                elem_dic[kc.key_payload] = meas.value
-                elem_dic[kc.key_address] = meas.channel
-                elem_dic[kc.key_timestamp] = meas.timestamp
-                elem_dic[kc.key_qos] = "good"
-                dic_list.append(elem_dic)
-                meas.json = 1
-        return dic_list
-
-    # Ritorna media delle misure per singolo canale e stato
-    def avg_by_channel(self, channel):
-
-        # Numero misure contate
-        val_count = 0
-        val_tot = 0
-        val_ts = 0
-        val_avg = 0
-
-        # Estraggo le misure e calcolo la media
-        for meas in self.plist:
-            if ((meas.channel == channel) & (meas.average == 0)):
-                if (val_ts == 0):
-                    val_ts = meas.timestamp
-                val_count = val_count + 1
-                val_tot = val_tot + meas.value
-                meas.average = 1
-
-        # Calcolo la media delle ultime misure
-        if (val_count > 0):
-            val_avg = val_tot / val_count
-        else:
-            val_avg = 0
-
-        meas_avg = Measure(channel, val_avg, val_ts, val_count)
-
-        return meas_avg
-
-    # Elimina gli elementi processati
-    def clear_list(self):
-        for meas in self.plist:
-            if (meas.json == 1):
-                self.plist.remove(meas)
-
-    # Ritorna la lista completa
-    def list(self):
-        return self.plist
-
 
 # Classe per eseguire la calibrazione iniziale
 class Calibration(object):
@@ -227,7 +128,7 @@ class StartThread(threading.Thread):
 
         global exit_flag
         global measure_list
-        global kc
+        global mkc
 
         while counter:
             # Verifico se ho premuto il pulsante di stop
@@ -255,9 +156,9 @@ class StartThread(threading.Thread):
 
             # Genero il JSON
             main_dic = {}
-            main_dic[kc.key_timestamp] = time.time()
-            main_dic[kc.key_qos] = "good"
-            main_dic[kc.key_values] = measure_list.json_dictionary()
+            main_dic[mkc.key_timestamp] = time.time()
+            main_dic[mkc.key_qos] = "good"
+            main_dic[mkc.key_values] = measure_list.json_dictionary()
 
             measure_list.clear_list()
 
@@ -302,13 +203,12 @@ class StartThread(threading.Thread):
         # Coloro il display in tinta unita
         sense.set_pixels(one_level)
 
-# Inizializzo le costanti del JSON
-kc = KeysConstant()
 sense.set_pixels(green_sign)
+mkc = mod_constants.MQTTKeysConstant()
 
 # Eseguo la calibrazione iniziale
 calib_temp = Calibration("SenseHat-Temp")
-measure_list = MeasureList()
+measure_list = mod_measure_list.MeasureList()
 
 # Create new threads
 th_acquisition = StartThread(1, "Acquisition", 2, 500)
